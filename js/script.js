df64a9109f5d76f6f6a3b76cc55beca69746b1b7
unknown <PR324333@L-777726113.wipro.com>
2015-11-16 07:07:26 +0530

    Almost done- draft1

--- js/script.js
+++ js/script.js
@@ -8,15 +8,3 @@ Updated: August 2013
 Notes:	 Hand coded for Udemy.com
 
 */
-
-$(function() {
-
-	$('#alertMe').click(function(e) {
-
-		e.preventDefault();
-
-		$('#successAlert').slideDown();
-
-	});
-
-});
